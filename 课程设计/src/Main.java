4b577ea669e93c544da002370f51feefcf2102a4
apple <qweqwe>
2019-12-19 21:08:39 +0800

    puyakun-ttttt

--- 课程设计/src/Main.java
+++ 课程设计/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("java课程设计");
         System.out.println("puyakun");
+        System.out.println("wshi ");
     }
 }
