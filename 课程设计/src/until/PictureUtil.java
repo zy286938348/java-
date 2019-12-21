2fe145b8a902b658bdff6a740afc5596c29b3699
张毅 <286938348@qq.com>
2019-12-21 20:22:37 +0800

    张毅-对Picture工具类的修改

--- 课程设计/src/until/PictureUtil.java
+++ 课程设计/src/until/PictureUtil.java
@@ -1,23 +1,35 @@
 package until;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.Vector;
 
 /**
  * 通过传入路径获取到图片
  */
 public class PictureUtil {
-    public static Image getImage(String path) {
-        BufferedImage bi = null;
-        try {
-            URL u = PictureUtil.class.getClassLoader().getResource(path);
-            bi = ImageIO.read(u);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bi;
+    public static ImageIcon getImage(String path) {
+        return new ImageIcon(PictureUtil.class.getResource(path));
+    }
+}
+class IconModel extends DefaultTableModel {
+    private static final long serialVersionUID = 1L;
+
+    public IconModel(Object[] cnames, int row) {
+        super(cnames, row);
+    }
+
+    public IconModel(Object[][] data, Object[] columnNames) {
+        super(data, columnNames);
+    }
+    //important here
+    public Class<?> getColumnClass(int col) {
+        Vector<?> v = (Vector<?>) dataVector.elementAt(0);
+        return v.elementAt(col).getClass();
+    }
+
+    public boolean isCellEditable(int row, int col) {
+        Class<?> columnClass = getColumnClass(col);
+        return columnClass != ImageIcon.class;
     }
 }
